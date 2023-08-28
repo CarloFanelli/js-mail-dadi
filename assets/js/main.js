685f517c3d85affcebd5bf6c5676662689b45fc1
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-08-28 15:00:47 +0200

    add MAIL part that work in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 /* Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
+// DADI
+
 // Math.random() * (max - min) + min -- per avere un numero random tra due parametri
 
 // creo il numero dell'utente
@@ -26,4 +28,33 @@ if (userNumber > computerNumber) {
 
 }{
     
+}
+
+
+// MAIL
+
+/* Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+ */
+
+// chiedo la mail all'utente
+const userMail = prompt('inserisci la tua email:');
+
+// lista di mail che possono accedere
+const mailsList = ['carlo@gmail.com','pino@gmail.com','gino@gmail.com','piero@gmail.com','andrea@gmail.com','luigi@gmail.com']
+
+// ciclo di controllo nella lista
+for (let i = 0; i  < mailsList.length; i++) {
+
+    const mailList = mailsList[i];
+
+    //console.log(mailList);
+
+    if (userMail === mailList) {
+        console.log('puoi entrare');
+        // assegno ad i il valore "finale" in modo che termini il ciclo una volta che trova l'elemento in lista
+        i = mailsList.length
+    }else{
+        console.log('respinto');
+    }
+
 }
